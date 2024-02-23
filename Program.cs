40de276a1f71f7786969e1ad898b8e11a79369d3
User <User@WIN-4QK4E4D33BC>
2024-02-23 18:07:11 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter operator (+,-,*, / )");
+char oper = Convert.ToChar(Console.ReadLine());
+
+if (oper == '+')
+
+{
+Console.WriteLine(num1 + num2);
+}
+else if (oper == '-')
+{
+Console.WriteLine(num1 - num2);
+
+}
+else if (oper == '*')
+{
+Console.WriteLine(num1 * num2);
+}
+else if (oper == '/')
+{
+    Console.WriteLine(num1 / num2 != 0);
+   
+}
+ else
+    {
+        Console.WriteLine("Error");
+    }
